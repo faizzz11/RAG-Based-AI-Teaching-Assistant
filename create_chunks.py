a2758022b5aed7243b03f5ce2b4762598a545b4e
faizzz11 <faizmoulavi54@gmail.com>
2025-09-04 14:53:26 +0530

    written the code to transcribe all the video files into chunks and texts and them creating respected json files for the respected audio file, with chucks and raw text in that audio

---
+++ create_chunks.py
@@ -0,0 +1,33 @@
+import whisper
+import os
+import json
+
+model = whisper.load_model("large-v2") #dowloading the whisper model which generates english text from different languages audios too
+audios = os.listdir("audios")
+
+for audio in audios:
+    #print(audio)
+    if("_" in audio):
+        number = audio.split("_")[0]
+
+        title = audio.split("_")[1][:-4]
+        print(number,title)
+        result = model.transcribe(audio= f"audios/{audio}", language="hi", task="translate", word_timestamps=False)
+        chunks = []
+        for segment in result['segments']:
+            chunks.append({
+            "number":number,
+            "title":title,   
+            "id":segment['id'],
+            "start":segment['start'],
+            "end":segment['end'],
+           "text":segment['text']
+            })
+
+        chunks_with_metadata = {"chunks":chunks, "text": result['text']}
+
+        print(chunks)
+        with open(f"jsons/{number} {title}.json", "w", encoding="utf-8") as f:
+            json.dump(chunks_with_metadata, f)
+        
+        
